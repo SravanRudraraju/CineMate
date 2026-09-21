4737b1272f943485eaf78d967f39928e077f687b
SravanRudraraju <sravanrudraraju@gmail.com>
2026-09-21 17:31:48 +0530

    feat: add user action tables

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,3 +5,28 @@ CREATE TABLE users (
     password_hash VARCHAR(255) NOT NULL,
     profile_image TEXT
 )
+
+CREATE TABLE watchlist(
+    user_id INTEGER NOT NULL,
+    tmdb_movie_id INTEGER NOT NULL,
+
+    PRIMARY KEY (user_id , tmdb_movie_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE liked_movies(
+    user_id INTEGER NOT NULL,
+    tmdb_movie_id INTEGER NOT NULL,
+
+    PRIMARY KEY (user_id , tmdb_movie_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE watched_movies(
+    user_id INTEGER NOT NULL,
+    tmdb_movie_id INTEGER NOT NULL,
+
+    PRIMARY KEY (user_id , tmdb_movie_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
